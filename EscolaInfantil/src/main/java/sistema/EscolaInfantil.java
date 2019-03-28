5a38497e11286482587d2e8529a267db9d91e4d9
Guilherme Esdras <guilherme.esdras@outlook.com>
2019-03-28 09:48:15 -0300

    EscolaInfantil

---
+++ EscolaInfantil/src/main/java/sistema/EscolaInfantil.java
@@ -0,0 +1,13 @@
+package sistema;
+
+/**
+ * Hello world!
+ *
+ */
+public class EscolaInfantil
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ EscolaInfantil/src/test/java/sistema/EscolaInfantilTest.java
@@ -0,0 +1,20 @@
+package sistema;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple EscolaInfantil.
+ */
+public class EscolaInfantilTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
