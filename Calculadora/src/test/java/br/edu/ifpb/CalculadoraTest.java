e95a69181c0d35e53b8c2e45a2da209fe1345440
Guilherme Esdras <guilherme.esdras@outlook.com>
2019-03-28 09:08:58 -0300

    add Calculadora

---
+++ Calculadora/src/test/java/br/edu/ifpb/CalculadoraTest.java
@@ -0,0 +1,27 @@
+package br.edu.ifpb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Calculadora.
+ */
+public class CalculadoraTest
+{
+    @Test
+    public void somar() {
+    }
+
+    @Test
+    public void subtrair() {
+    }
+
+    @Test
+    public void multiplicar() {
+    }
+
+    @Test
+    public void dividir() {
+    }
+}
